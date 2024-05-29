9d6e6d8bab10b8d2957dafc739f8aa224752a566
Josh Dietrich <jdietrich0603@gmail.com>
2024-05-29 12:43:51 -0400

    Adding keybinds to jump to next and previous warning or error with LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -31,6 +31,10 @@ return {
 				vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
 				vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
 				vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+
+				vim.keymap.set("n", "g]", vim.diagnostic.goto_next, {desc="Go to next warning"})
+				vim.keymap.set("n", "g[", vim.diagnostic.goto_prev, {desc="Go to previous warning"})
+
 			end
 		})
 
