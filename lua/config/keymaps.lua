1af5ecb801f87455a9757bbd0644073fabb57f4d
Josh Dietrich <joshuadietrich256@gmail.com>
2024-09-05 13:32:23 -0400

    removing the alt behaviour
    the alt behaviour along with tmux makes pressing esc and j or k also
    move lines. I don't know why this is. Look for a better solution

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,12 @@
 local wk = require('which-key')
 
--- that alt move behaviour
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
-vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
-vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+-- -- that alt move behaviour
+-- vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
+-- vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
+-- vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
+-- vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
+-- vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+-- vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
 
 -- floating terminal
 vim.keymap.set("n", "<C-_>", ":FloatermToggle<CR>")
