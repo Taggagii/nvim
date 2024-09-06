b65de78c4619584ef59b0354be4482737822f372
Taggagii <jdietrich@gmail.com>
2024-09-05 23:32:00 -0400

    dashboard but minimal

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -2,15 +2,7 @@ return {
 	"nvimdev/dashboard-nvim",
 	lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
 	opts = function()
-	local logo = [[]]
--- 		local logo = [[
--- 888b    |                    Y88b      / ,e,               
--- |Y88b   |  e88~~8e   e88~-_   Y88b    /   "  888-~88e-~88e 
--- | Y88b  | d888  88b d888   i   Y88b  /   888 888  888  888 
--- |  Y88b | 8888__888 8888   |    Y888/    888 888  888  888 
--- |   Y88b| Y888    , Y888   '     Y8/     888 888  888  888 
--- |    Y888  "88___/   "88_-~       Y      888 888  888  888 
---     ]]
+		local logo = [[work]]
 
 		logo = string.rep("\n", 8) .. logo .. "\n\n"
 		local builtin = require("telescope.builtin")
