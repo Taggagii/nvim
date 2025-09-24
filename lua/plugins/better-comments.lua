c701894722fa2a6a5a7d3567daa660488216a46b
Taggagii <josh.dietrich@d2l.com>
2025-09-24 11:12:59 -0400

    better comments like vscode

---
+++ lua/plugins/better-comments.lua
@@ -0,0 +1,43 @@
+return {
+	"Djancyp/better-comments.nvim",
+	config = function()
+		require("better-comment").Setup({
+			tags = {
+				{
+					name = "todo",
+					fg = "black",
+					bg = "yellow",
+					bold = true,
+					virtual_text = "",
+				},
+				{
+					name = "!",
+					fg = "#f44747",
+					bg = "",
+					bold = true,
+					virtual_text = "",
+				},
+				{
+					name = "?",
+					fg = "#add8e6",
+					bg = "",
+					bold = true,
+					virtual_text = "?",
+				},
+				{
+					name = "*",
+					fg = "yellow",
+					bg = "",
+					bold = true,
+					virtual_text = "",
+				}
+			}
+
+		})
+	end
+}
+
+-- todo : something that I need to do
+-- ? : some question
+-- ! : this should be fixed
+-- * : pay attention
