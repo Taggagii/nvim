9185822a86bc9d1ab9300cb11f547848f8cfcd97
Josh Dietrich <joshuadietrich256@gmail.com>
2025-10-14 16:28:44 -0400

    trying to get indents to be 4 by default

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,12 +1,15 @@
 require("config.keymaps")
 
--- vim.cmd("set shiftwidth=4")
--- vim.cmd("set tabstop=4")
+vim.g.editorconfig = false
+
+vim.cmd("set shiftwidth=4")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4 noexpandtab")
 vim.cmd("set scrolloff=40")
 vim.cmd("set ignorecase smartcase")
 
 vim.opt.number = true
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
 
 vim.opt.clipboard:append("unnamedplus")
@@ -23,5 +26,4 @@ vim.diagnostic.config({
   signs = true,         -- Show signs in the gutter
   update_in_insert = false,
 })
-
 
