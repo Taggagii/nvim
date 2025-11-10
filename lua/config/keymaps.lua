7b4564524543e4c2f434770b640e1a46362cf509
Taggagii <Jodietrich@desire2learn.com>
2025-11-10 16:35:09 -0500

    adding searching and adding some keybinds

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+-- Useful keymaps registered with which-key format
+local wk = require('which-key')
+
+wk.add({
+	-- FzfLua group
+	{ '<leader>f', group = 'FzfLua' },
+	{ '<leader>ff', function() vim.cmd('FzfLua files') end, desc = 'Search Files' },
+	{ '<leader>fg', function() vim.cmd('FzfLua live_grep') end, desc = 'Grep Files' },
+	{ '<leader>fb', function() vim.cmd('FzfLua buffers') end, desc = 'Search Buffers' },
+	{ '<leader>fh', function() vim.cmd('FzfLua help_tags') end, desc = 'Search Help Tags' },
+	{ '<leader>fm', function() vim.cmd('FzfLua marks') end, desc = 'Search Marks' },
+	{ '<leader>fc', function() vim.cmd('FzfLua commands') end, desc = 'Search Commands' },
+	{ '<leader>fk', function() vim.cmd('FzfLua keymaps') end, desc = 'Search Keymaps' },
+	{ '<leader>fa', function() vim.cmd('FzfLua builtin') end, desc = 'FzfLua Builtins' },
+
+	-- Oil file explorer (no group)
+	{ '<leader>e', function() vim.cmd('Oil') end, desc = 'Open File System' },
+})
