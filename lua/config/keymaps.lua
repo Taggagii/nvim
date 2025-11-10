0886fdc0348355f0c74223c305ed0604425007fc
Taggagii <Jodietrich@desire2learn.com>
2025-11-10 16:56:41 -0500

    trying to get trouble working (for errors)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,5 +18,13 @@ wk.add({
 
 	-- Style
 	{ '<leader>s', group='Style' },
-	{ '<leader>sf', function() vim.cmd('LspEslintFixAll') end, desc = 'Eslint Fix All' }
+	{ '<leader>sf', function() vim.cmd('LspEslintFixAll') end, desc="Eslint Fix All" },
+
+	-- Trouble
+	{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics", },
+	{ "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics", },
+	{ "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols", },
+	{ "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / references / ...", },
+	{ "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List", },
+	{ "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List", },
 })
