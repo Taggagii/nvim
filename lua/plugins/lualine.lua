561278abbd7cc46fc749944d22d6c75db81e1b6a
Taggagii <Jodietrich@desire2learn.com>
2025-12-15 10:31:55 -0500

    lualine and neckpain width

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,28 @@ return {
 	'nvim-lualine/lualine.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	config = function()
-		require('lualine').setup()
+		require('lualine').setup({
+			sections = {
+				lualine_b = {},
+				lualine_c = {
+					{
+						'filename',
+						path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path, 3 = absolute with tilde for home
+					}
+				},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = { 'branch' }
+			},
+			inactive_sections = {
+				lualine_c = {
+					{
+						'filename',
+						path = 1,
+					}
+				},
+				lualine_x = {}
+			}
+		})
 	end
 }
--- lua/plugins/neck-pain.lua
+++ lua/plugins/neck-pain.lua
@@ -3,6 +3,7 @@ return {
 	event = "VeryLazy", -- this lazy loads the plugin on a specific event
 	opts = {
 		autocmd = true, -- automatically enable on startup
+		width = 150,
 		-- add other NoNeckPain options here if desired
 	}
 }
