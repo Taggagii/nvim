cb0cc0d08ddca3a8bf28ba7315cac7fef3b1262c
Josh Dietrich <jdietrich0603@gmail.com>
2026-01-08 20:29:17 -0500

    leaving the default signs as they are

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,13 +4,6 @@ return {
 	"lewis6991/gitsigns.nvim",
 	lazy = false,
 	opts = {
-		-- signs = {
-		-- 	add          = { text = "+" },
-		-- 	change       = { text = "~" },
-		-- 	delete       = { text = "_" },
-		-- 	topdelete    = { text = "‾" },
-		-- 	changedelete = { text = "~" },
-		-- },
 		current_line_blame = true,
 	},
 }
