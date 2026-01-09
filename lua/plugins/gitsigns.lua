4c3ce29b981ead661295d4252a01b376c987fb96
Josh Dietrich <jdietrich0603@gmail.com>
2026-01-08 20:27:23 -0500

    trying to get git changes working

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,16 @@
+-- to show changed lines in a file
+
+return {
+	"lewis6991/gitsigns.nvim",
+	lazy = false,
+	opts = {
+		-- signs = {
+		-- 	add          = { text = "+" },
+		-- 	change       = { text = "~" },
+		-- 	delete       = { text = "_" },
+		-- 	topdelete    = { text = "‾" },
+		-- 	changedelete = { text = "~" },
+		-- },
+		current_line_blame = true,
+	},
+}
--- lua/plugins/unused-tpope-vim-sleuth.lua
+++ lua/plugins/unused-tpope-vim-sleuth.lua
@@ -1,4 +1,5 @@
 return {}
+-- that's something new
 -- return {
 -- 	"tpope/vim-sleuth"
 -- };
