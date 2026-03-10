e988f0438f307f2103e6b4b212978cf6418b18a9
Taggagii <jdietrich@gmail.com>
2026-03-09 20:31:57 -0400

    fixing my copying on windows stuff, hoping it still works on laptop

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -10,6 +10,18 @@ vim.opt.signcolumn = 'yes'
 
 -- for copying
 vim.opt.clipboard:append('unnamedplus')
+vim.g.clipboard = {
+  name = "win32yank",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+  cache_enabled = 0,
+}
 
 -- for matting
 local indentAmount = 2
