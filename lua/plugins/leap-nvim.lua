7a9e34db11d4aa918db2fda313ed15d38a40d0fd
Taggagii <jdietrich0603@gmail.com>
2026-04-08 16:31:37 -0400

    adding those leap keybinds

--- lua/plugins/leap-nvim.lua
+++ lua/plugins/leap-nvim.lua
@@ -3,5 +3,7 @@ return {
 	lazy = false,
 	config = function()
 		require("leap").add_default_mappings()
+    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)', { silent = true, noremap = false })
+    vim.keymap.set('n', 'S', '<Plug>(leap-from-window)', { silent = true, noremap = false })
 	end
 }
